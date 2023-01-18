5627037937407a121be78a57fc02708d62f2f648
DumbChester021 <122507103+DumbChester021@users.noreply.github.com>
2023-01-18 21:16:42 +0800

    Added Looping Card Code Makes it easier to modify the contents

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-const product1Button = document.getElementById("product1Button");
-const product2Button = document.getElementById("product2Button");
-const product3Button = document.getElementById("product3Button");
-const product4Button = document.getElementById("product4Button");
 
-product1Button.addEventListener("click", () => {
-  window.location.href = "products.html?product=1&name=Barley+Capsule";
-});
 
-product2Button.addEventListener("click", () => {
-  window.location.href = "products.html?product=2&name=Fusion+Coffee";
-});
+fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        let productsContainer = document.getElementById("productsContainer");
+        data.products.forEach(product => {
+            let col = document.createElement("div");
+            col.classList.add("col", "py-2");
 
-product3Button.addEventListener("click", () => {
-  window.location.href = "products.html?product=3&name=Boost+Coffee";
-});
-
-product4Button.addEventListener("click", () => {
-  window.location.href = "products.html?product=4&name=Barley+Powder";
-});
+            let card = document.createElement("div");
+            card.classList.add("card");
+            card.style.width = "18rem";
+            
+            let img = document.createElement("img");
+            img.src = product.imgSrc;
+            img.classList.add("card-img-top");
+            img.alt = product.name;
+            card.appendChild(img);
+            
+            let cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+            
+            let productName = document.createElement("h5");
+            productName.classList.add("card-title");
+            productName.innerHTML = product.name;
+            cardBody.appendChild(productName);
+            
+            let productDescription = document.createElement("p");
+            productDescription.classList.add("card-text");
+            productDescription.innerHTML = product.description;
+            cardBody.appendChild(productDescription);
+            
+            let button = document.createElement("button");
+            button.type = "button";
+            button.classList.add("btn", "btn-primary");
+            button.innerHTML = "View Product";
+            cardBody.appendChild(button);
+            
+            card.appendChild(cardBody);
+            col.appendChild(card);
+            productsContainer.appendChild(col);
+        });
+    });

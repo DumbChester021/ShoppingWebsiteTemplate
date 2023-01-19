069db0ce9872bdc3f13e138809190c462c5e4d6f
DumbChester021 <122507103+DumbChester021@users.noreply.github.com>
2023-01-19 19:22:32 +0800

    Added TelegramBot Integration Added Telegram Integration when placing an order also fixed some minor Bugs with modal

--- products.js
+++ products.js
@@ -3,6 +3,7 @@ const productId = urlParams.get("product");
 
 var productImg = document.getElementById("productImg");
 var productName = document.getElementById("productName");
+var orderName = document.getElementById("orderName");
 var productDescription = document.getElementById("productDescription")
 var productPrice = document.getElementById("productPrice");
 var bloatedPrice = document.getElementById("bloatedPrice")
@@ -18,10 +19,12 @@ fetch('products.json')
         if (product) {
             productImg.src = product.imgSrc;
             productName.innerHTML = product.name;
+            orderName.innerHTML = product.name;
             productDescription.textContent = product.description;
             productPrice.innerHTML = "₱" + product.price + ".00";
             bloatedPrice.innerHTML = "₱" + (product.price + ( product.price *  0.2 ));
             skuId.innerHTML = "SKU: JS-"+ product.id;
+            
         }
     });
 
@@ -37,6 +40,42 @@ fetch('products.json')
 
     // Add an event listener to the close button to close the modal when clicked
     var closeButton = document.querySelector(".btn-close");
+    closeButton.addEventListener("click", function() {
+        shippingModal.classList.remove("show");
+        shippingModal.style.display = "none";
+    });
+
+    // Add an event listener to the Place Order button to Send Notifications to TG
+    var placeOrderButton = document.getElementById("placeOrderButton");
+    placeOrderButton.addEventListener("click", function() {
+        // Get the values of the form inputs
+        var name = document.getElementById("name").value;
+        var mobileNumber = document.getElementById("mobileNumber").value;
+        var address = document.getElementById("address").value;
+        var brgy = document.getElementById("brgyStreet").value;
+        var city = document.getElementById("townCity").value;
+        var province = document.getElementById("stateProvince").value;
+        var region = document.getElementById("region").value;
+        var country = document.getElementById("country").value;
+        var postalCode = document.getElementById("postalCode").value;
+        var shippingNotes = document.getElementById("shippingNotes").value;
+        var productName = document.getElementById("productName").innerHTML;
+        var productPrice = document.getElementById("productPrice").innerHTML;
+        var bloatedPrice = document.getElementById("bloatedPrice").innerHTML;
+    
+        // Construct the message to send
+        var msg = "New Order:%0AName: " + name + "%0AMobile Number: " + mobileNumber + "%0AAddress: " + address + ", " + brgy + ", " + city + ", " + province + ", " + region + ", " + country + "%0APostal Code: " + postalCode + "%0AShipping Notes: " + shippingNotes + "%0AProduct Name: " + productName + "%0AProduct Price: " + productPrice + "%0ABloated Price: " + bloatedPrice;
+    
+        // Send the message to Telegram
+        var telegramUrl = "https://api.telegram.org/bot5319457642:AAFIlKkH6IsLqfUGd2RvI8GVBRtl_2FUaQE/sendMessage?chat_id=@BreakSoftOfficial&text=" + '"' + msg +'"';
+    
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', telegramUrl);
+        xhr.send();
+    });
+
+    // Add an event listener to the Cancel Order button to close the modal when clicked
+    var closeButton = document.getElementById("cancelOrderButton")
     closeButton.addEventListener("click", function() {
         shippingModal.classList.remove("show");
         shippingModal.style.display = "none";
